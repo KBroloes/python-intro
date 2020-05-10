50ba6d5e5597b517b28c2964aeb608d99dac2532
Kevin Broløs <kevin.broloes@gmail.com>
2020-05-10 17:32:29 +0200

    Add solution

--- tictacpy/board.py
+++ tictacpy/board.py
@@ -10,40 +10,41 @@ win_conditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontals
                  ]
 
 # A list of the pretty names for valid locations
-# Note: The tests use this list for testing various functions. You can change the names, but need to expose the variable like this.
+# Note: The tests use this list for testing various functions. You can change the names, but need to expose the variable.
 valid_locations = ['TopLeft', 'Top', 'TopRight',
                     'Left', 'Center', 'Right',
                     'BottomLeft', 'Bottom', 'BottomRight']
 
 # The Board class you will be implementing
 class Board(BaseBoard):
-    # Initial state is None for each of the valid locations
-    state = dict({'TopLeft': None, 'Top': None, 'TopRight': None,
-                    'Left': None, 'Center': None, 'Right': None,
-                    'BottomLeft': None, 'Bottom': None, 'BottomRight':None
-                })
-    current_move = 'X'
-
     def __init__(self):
         self.reset()
 
 
-    def pass_turn(self):
-        """ Passes the turn by setting the 'current_move' attribute to the other player
 
-            Hint: Remember self. in front of class variables. """
-        # Implement me!
-        pass
+    def _get_display_value(self, value):
+        if value is None:
+            return ' '
+        else:
+            return value
+
+
+    def pass_turn(self):
+        """ Passes the turn by setting the 'current_move' attribute to the other player """
+        if self.current_move == 'X':
+            self.current_move = 'O'
+        else:
+            self.current_move = 'X'
+
+
+    def _valid_location(self, location):
+        return self.state[location] is None
 
 
     def available_locations(self):
-        """ lists the locations on the board not yet occupied by a piece
-
-            Hint: you can check if a value is taken by example:
-                    self.state['Top'] is None
-        """
-        # Implement me!
-        pass
+        """ lists the locations on the board not yet occupied by a piece """
+        # Filter by value == None, return the key.
+        return list(map(lambda x: x[0], filter(lambda x: x[1] is None, self.state.items())))
 
 
     def set_piece(self, location):
@@ -52,68 +53,81 @@ class Board(BaseBoard):
                 location - the location on the board to set the piece
             Exceptions:
                 Raises an exception if the location is invalid (already taken)
-
-            Hint: Remember to validate if the location is taken in the state dict.
-                  Dicts can be assigned using ie. self.state['Top'] = <value>
         """
-        # Implement me!
-        pass
+        if self._valid_location(location):
+            self.state[location] = self.current_move
+        else:
+            raise Exception(f"Bad location {location} for player {self.current_move}")
 
 
     def reset(self):
         """ Resets the board so all locations are unoccupied and sets the current player to player 1 (X) """
-        # Implement me!
-        pass
+        # Create the board state as a dictionary with values from 0-8 (positions of the board)
+        # All initialized with None values
+        self.state = dict([(i, None) for i in valid_locations])
+
+        # Whose turn is it?
+        self.current_move = 'X'
 
 
     def display(self):
-        """ Displays (prints) the current board state
+        """ Displays (prints) the current board state """
+        # Convert dict values to list
+        values = list(self.state.values())
+        for i in range(3):
+            # Let's learn about slices:
+            current_position = i*3
+            next_three = current_position+3
 
-            Hint: Remember it should be displayed as a 3x3 grid.
-                You can loop over the items 3 by 3 in the state dict to accomplish this using the print function.
+            # Create a slice of three elements (one row) using [a:b]
+            row = values[current_position:next_three]
+            # Use a list comprehension to convert None to ' '
+            row = [self._get_display_value(value) for value in row]
 
-                Maybe replace "None" values with a blank string for the display?
-                Example (but you can display how you want):
-                X |   |
-                    | O |
-                    |   | X
+            print(row)
+
+
+    def _all_met(self, condition):
+        """ This is a reduce function as a loop.
+            Returns true if all locations in a win condition are occupied by the current player
         """
-        # Implement me!
-        pass
+        all_set = True
+        for location in condition:
+            # Translate to the pretty locations
+            loc = valid_locations[location]
+
+            # Figure out if the current player symbol is set
+            all_set = all_set and (self.state[loc] == self.current_move)
+
+        return all_set
 
 
     def check_win_condition(self):
         """ Returns True if the win condition is met anywhere on the board.
             Note we only have to check the current player's token for each turn,
-            as the other player will be checked on subsequent or previous turns.
+            as the other player will be checked on subsequent or previous turns. """
 
-            Hint: You need to check each of the win conditions.
-                This is typically easiest just looping over the configurations in the start of this file:
-                win_conditions.
-                These are from values 0-8 from top left to bottom right in the 3x3 grid,
-                so you need to translate to the pretty names we have in the state.
-                You can use the valid_locations list for that.
-            """
-        # Implement me!
-        pass
+        for condition in win_conditions:
+            if self._all_met(condition):
+                return True
+
+        return False
 
 
     def all_filled(self):
         """ Simply returns True if all spots are taken.
-            Expects win condition to be checked elsewhere.
+            Expects win condition to be checked elsewhere. """
+        for item in self.state.values():
+            if item is None:
+                return False
 
-            Hint: Just check if all the spots are not None """
-        # Implement me!
-        pass
+        return True
 
 
     def save(self, filename):
-        """ Saves the board as a file
-
-            Hint: Check the examples folder in the file_system.py for examples on how to save
-        """
-        # Implement me!
-        pass
+        """ Saves the board as a file """
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
 
 
     @staticmethod
@@ -121,9 +135,6 @@ class Board(BaseBoard):
         """ Loads the board from file
             Returns:
                 Board instance
-
-
-            Hint: Check the examples folder in the file_system.py for examples on how to load
         """
-        # Implement me!
-        pass
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
